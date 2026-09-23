ac4c88a352c49156c9d30cc6ce6b51e6671b8ca7
lgabor <gabor.lignier@ynov.com>
2026-09-23 15:28:46 +0200

    {ADD}Data

--- escape_engine_api/app/data.py
+++ escape_engine_api/app/data.py
@@ -3,11 +3,13 @@ players = [
         "id": 1,
         "name": "Gilles",
         "score": 0,
+        "life": True,
     },
     {
         "id": 2,
         "name": "Pierrot",
         "score": 0,
+        "life": True,
     },
  
 ]
