18d70477c9fdde3c2c9c30082b89c772bae50940
lgabor <gabor.lignier@ynov.com>
2026-09-23 15:29:58 +0200

    {ADD}inventaire

---
+++ escape_engine_api/app/inventory.py
@@ -0,0 +1,25 @@
+class Inventaire:
+	def __init__(self):
+		self.objets = []
+
+	def ajouter(self, objet):
+		self.objets.append(objet)
+
+	def retirer(self, nom_objet):
+		for objet in self.objets:
+			if objet.get_nom().lower() == nom_objet.lower():
+				self.objets.remove(objet)
+				return objet
+		return None
+
+	def est_vide(self):
+		return not self.objets
+
+	def afficher(self):
+		if self.est_vide():
+			print("Inventaire vide.")
+			return
+
+		print("Inventaire :")
+		for objet in self.objets:
+			print(f"- {objet.get_nom()}")
